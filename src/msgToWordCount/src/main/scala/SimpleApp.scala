d5249c41144fd3b863f17fd8680cfe377eb87f4c
kelinliu <kelin.liu@avidlifemedia.com>
2016-03-03 21:27:12 -0500

    msg sample: word count

--- src/msgToWordCount/src/main/scala/SimpleApp.scala
+++ src/msgToWordCount/src/main/scala/SimpleApp.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SQLContext}
 
 object SimpleApp {
-  val url = "jdbc:mysql://bigdata-master:3306/ashley"
+  val url = "jdbc:mysql://bigdata-master:3306/nlp"
   val driver = "com.mysql.jdbc.Driver"
   val user = System.getenv("MYSQL_USERNAME")
   val pwd = System.getenv("MYSQL_PASSWORD")
@@ -18,30 +18,34 @@ object SimpleApp {
     val df = sqlContext.read.format("jdbc").
       option("url", url).
       option("driver", driver).
-      option("dbtable", "chat_message").
+      option("dbtable", "msg").
       option("user", user).
       option("password", pwd).
       load()
-    df.registerTempTable("chat")
+    df.registerTempTable("t_msg")
 
-    val msgDF = sqlContext.sql("select msg from chat")
+    val msgDF = sqlContext.sql("select message from t_msg")
+    msgDF.printSchema()
 
     val cleaner = (msg: String) => {
       msg.toLowerCase.split(" ").map((w: String) => w.replaceAll("[^a-zA-Z0-9]", ""))
     }
-    val wordDF = msgDF.explode("msg", "word")((r: String) => cleaner(r))
+    val wordDF = msgDF.explode("message", "word")((r: String) => cleaner(r))
 
     wordDF.registerTempTable("words")
     val wordCount = sqlContext.sql("select word, count(1) as cnt from words group by word order by cnt desc")
     println(wordCount.count())
 
-    save(wordCount, "chat_message_word_count")
+    save(wordCount, "msg_word_count")
   }
 
   def save(dataFrame: DataFrame, table: String): Unit = {
     val props = new java.util.Properties()
     props.setProperty("user", user)
     props.setProperty("password", pwd)
-    org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils.saveTable(dataFrame, url, table, props)
+    props.setProperty("driver", driver)
+
+    // create and save in table
+    dataFrame.write.jdbc(url, table, props)
   }
 }
