a0074c8b189c1290f56ee502a656def380341601
Kelin Liu <kelin.liu@sears.ca>
2016-11-02 14:33:11 -0400

    csv to DF: check broken line

--- src/csvToDF/src/main/scala/SimpleApp.scala
+++ src/csvToDF/src/main/scala/SimpleApp.scala
@@ -14,16 +14,21 @@ object SimpleApp {
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    val df = sqlContext.read
+    val s7export = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true") // Use first line of all files as header
       .option("inferSchema", "false") // Automatically infer data types
-      .option("delimiter", "\t")
+      .option("delimiter", ";")
       .option("nullValue", "null")
       .option("treatEmptyValuesAsNulls", "true")
-      .load("file:///tmp/0.csv")
+      .load("file:///tmp/s7export-fix.csv")
 
-    df.show(3)
+    s7export.printSchema
+
+    val brokens = s7export.select("*").where(!s7export("<ID>").contains("_P") && !s7export("<ID>").contains("_I"))
+    brokens.show()
+    println(brokens.count)
+
 
     /*
     val selectedData = df.select("year", "model")
@@ -36,3 +41,12 @@ object SimpleApp {
       */
   }
 }
+
+/*
+val df = spark.sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "false").option("delimiter", "\t").option("nullValue", "null").option("treatEmptyValuesAsNulls", "true").load("file:///tmp/not_live.csv")
+ */
+
+
+/*
+/home/ubuntu/spark/bin/spark-shell --master spark://bigdata-master:7077 --packages  com.databricks:spark-csv_2.10:1.5.0, xxx:xxx:xxx
+ */
