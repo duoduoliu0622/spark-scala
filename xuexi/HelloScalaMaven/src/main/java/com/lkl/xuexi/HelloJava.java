ad0e5924aecaf3cbc790a044e1210d9d1bd9002c
kevin.liu <noteless0622@gmail.com>
2017-09-04 15:33:49 -0400

    ScalaMaven: initial commit.

---
+++ xuexi/HelloScalaMaven/src/main/java/com/lkl/xuexi/HelloJava.java
@@ -0,0 +1,8 @@
+package com.lkl.xuexi;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ xuexi/HelloScalaMaven/src/main/scala/com/lkl/xuexi/HelloScala.scala
@@ -0,0 +1,5 @@
+package com.lkl.xuexi;
+
+object HelloScala extends App {
+  println("Hello Scala")
+}
